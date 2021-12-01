3a8042ea73a54772a0145c00913bfb612a03374f
Marat Nova <mmaratn@yahoo.com>
2021-11-30 21:34:15 -0500

    Starting saucelabs.

--- src/test/java/endToEnd/TestBase.java
+++ src/test/java/endToEnd/TestBase.java
@@ -1,13 +1,16 @@
 package endToEnd;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.*;
 import pages.areas.InboxArea;
+import sun.net.www.content.text.PlainTextInputStream;
 
 public class TestBase
 {
@@ -21,6 +24,13 @@ public class TestBase
     @BeforeClass
     public void initialization()
     {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.setCapability("username", "powercat");
+        mutableCapabilities.setCapability("accessKey", "81ebdd4b-2c73-4dab-a5ae-9e05a52f5950");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("sauce:options", mutableCapabilities);
+        desiredCapabilities.setCapability("browserVersion", "latest");
+        desiredCapabilities.setCapability("platformName", "windows 7");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
